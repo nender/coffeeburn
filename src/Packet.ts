93c042d3d5d955f2e35beda91251a8ce6d256349
Nathan Ender <nender@users.noreply.github.com>
2020-08-16 17:21:03 +0200

    Remove force-unwrapping from packet

--- src/Packet.ts
+++ src/Packet.ts
@@ -1,16 +1,16 @@
 import { Hub } from "./Hub";
 
 export class Packet {
-    id!: number
-    isPOD!: boolean
-    target!: Hub
-    speed!: number
-    birthFrame!: number
+    id: number
+    isPOD: boolean
+    target: Hub
+    speed: number
+    birthFrame: number
     
     /** True if packet is currently travelling from A to B */
-    TAToB!: boolean
+    TAToB: boolean
     /** Float in the range 0<=x<1 indicating progress along current Pipe */
-    TProgress!: number;
+    TProgress: number;
 
     
     constructor(id: number, target: Hub, isPOD = false, speed: number, currentFrame: number) {
